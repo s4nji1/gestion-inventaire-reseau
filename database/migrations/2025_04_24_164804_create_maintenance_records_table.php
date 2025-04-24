84b88ce79ffc31dfcdab1d441cab9214feda7ac4
s4nji1 <hamzaaitbouhou75@gmail.com>
2025-04-24 23:10:53 +0100

    first

---
+++ database/migrations/2025_04_24_164804_create_maintenance_records_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('maintenance_records', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('equipment_id')->constrained()->onDelete('cascade');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->string('maintenance_type'); // Préventive, corrective, etc.
+            $table->text('issue_description');
+            $table->text('resolution_description')->nullable();
+            $table->enum('status', ['pending', 'in_progress', 'completed', 'cancelled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('maintenance_records');
+    }
+};
