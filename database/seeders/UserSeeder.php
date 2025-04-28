229e609ef04a087c6c899174b772b904814c9783
s4nji1 <hamzaaitbouhou75@gmail.com>
2025-04-28 11:53:55 +0100

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Hamza Aitbouhou',
+            'email' => 'hamzaaitbouhou75@gmail.com',
+            'password' => Hash::make('11111111'),
+        ]);
     }
 }
